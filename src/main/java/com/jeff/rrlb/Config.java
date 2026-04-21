f36f9081638be844f2b59d75e8ce34edce3517e3
downloadableduck <238381293+downloadableduck@users.noreply.github.com>
2026-04-21 11:55:35 -0700

    fix some stuffs

--- src/main/java/com/jeff/rrlb/Config.java
+++ src/main/java/com/jeff/rrlb/Config.java
@@ -1,13 +1,13 @@
 package com.jeff.rrlb;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     private static boolean validateItemName(final Object obj) {
-        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
+        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(Identifier.parse(itemName));
     }
 }
--- src/main/java/com/jeff/rrlb/mixin/RemoveTheActualBar.java
+++ src/main/java/com/jeff/rrlb/mixin/RemoveTheActualBar.java
@@ -7,8 +7,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LoadingOverlay.class)
-public class RemoveResourceLoadingBarMixin {
-    @Inject(at = @At("HEAD"), method = "drawProgressBar", cancellable = true)
+public class RemoveTheActualBar {
+    @Inject(at = @At("HEAD"), method = "extractProgressBar", cancellable = true)
     private void init(CallbackInfo info) {
         info.cancel();
     }
